117c008018e5179d92ca848ce89767ed34403877
Lukas <lukas-meier@mailbox.org>
2023-12-01 22:14:17 +0100

    fixed inconsistency in replacement strings, now: name[digit]name

--- day1/numbers.go
+++ day1/numbers.go
@@ -12,23 +12,23 @@ func replace(input string) string {
 		if strings.Contains(input, dn) {
 			switch dn {
 			case "one":
-				input = strings.Replace(input, digitNames[i], "one1ne", -1)
+				input = strings.Replace(input, digitNames[i], "one1one", -1)
 			case "two":
-				input = strings.Replace(input, digitNames[i], "two2", -1)
+				input = strings.Replace(input, digitNames[i], "two2two", -1)
 			case "three":
-				input = strings.Replace(input, digitNames[i], "three3", -1)
+				input = strings.Replace(input, digitNames[i], "three3three", -1)
 			case "four":
-				input = strings.Replace(input, digitNames[i], "four4", -1)
+				input = strings.Replace(input, digitNames[i], "four4four", -1)
 			case "five":
-				input = strings.Replace(input, digitNames[i], "five5", -1)
+				input = strings.Replace(input, digitNames[i], "five5four", -1)
 			case "six":
-				input = strings.Replace(input, digitNames[i], "six6", -1)
+				input = strings.Replace(input, digitNames[i], "six6six", -1)
 			case "seven":
-				input = strings.Replace(input, digitNames[i], "seven7", -1)
+				input = strings.Replace(input, digitNames[i], "seven7seven", -1)
 			case "eight":
-				input = strings.Replace(input, digitNames[i], "eight8", -1)
+				input = strings.Replace(input, digitNames[i], "eight8eight", -1)
 			case "nine":
-				input = strings.Replace(input, digitNames[i], "nine9", -1)
+				input = strings.Replace(input, digitNames[i], "nine9nine", -1)
 			}
 		}
 	}
