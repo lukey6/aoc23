221b7a6d5e2abba8ef37d720a2aa02442a2fa993
Lukas <lukas-meier@mailbox.org>
2023-12-01 22:09:39 +0100

    fixed replace() only replacing one instance of the digit name

--- day1/day1.go
+++ day1/day1.go
@@ -6,6 +6,9 @@ import (
 	"strings"
 )
 
+// Task: sum of first and last digit read as two digit number per line
+// Part 2: also english names for digits 1-9
+
 var exampleInputPart1 string = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet"
 var exampleInputPart2 string = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen\n"
 
--- day1/numbers.go
+++ day1/numbers.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 )
 
-var digitNames = []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+var digitNames = []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero"}
 
 // replaces spelled out digits with the digit strings
 func replace(input string) string {
@@ -12,23 +12,23 @@ func replace(input string) string {
 		if strings.Contains(input, dn) {
 			switch dn {
 			case "one":
-				input = strings.Replace(input, digitNames[i], "one1ne", 1)
+				input = strings.Replace(input, digitNames[i], "one1ne", -1)
 			case "two":
-				input = strings.Replace(input, digitNames[i], "two2", 1)
+				input = strings.Replace(input, digitNames[i], "two2", -1)
 			case "three":
-				input = strings.Replace(input, digitNames[i], "three3", 1)
+				input = strings.Replace(input, digitNames[i], "three3", -1)
 			case "four":
-				input = strings.Replace(input, digitNames[i], "four4", 1)
+				input = strings.Replace(input, digitNames[i], "four4", -1)
 			case "five":
-				input = strings.Replace(input, digitNames[i], "five5", 1)
+				input = strings.Replace(input, digitNames[i], "five5", -1)
 			case "six":
-				input = strings.Replace(input, digitNames[i], "six6", 1)
+				input = strings.Replace(input, digitNames[i], "six6", -1)
 			case "seven":
-				input = strings.Replace(input, digitNames[i], "seven7", 1)
+				input = strings.Replace(input, digitNames[i], "seven7", -1)
 			case "eight":
-				input = strings.Replace(input, digitNames[i], "eight8", 1)
+				input = strings.Replace(input, digitNames[i], "eight8", -1)
 			case "nine":
-				input = strings.Replace(input, digitNames[i], "nine9", 1)
+				input = strings.Replace(input, digitNames[i], "nine9", -1)
 			}
 		}
 	}
